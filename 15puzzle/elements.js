f06c4cf77803354ce8c98cb774370ea54f838677
czyzi0 <czyznikiewicz.mateusz@gmail.com>
2021-05-04 15:33:36 +0200

    Add more or less working game of 15Puzzle

--- 15puzzle/elements.js
+++ 15puzzle/elements.js
@@ -1,26 +1,50 @@
 class Tile {
 
-  constructor(imageIdx, x, y, val, size) {
+  constructor(imageIdx, x, y, val, size, n) {
     this._size = size;
+    this._n = n;
+
     this._x = x;
     this._y = y;
+
+    this._X = (n - 1) / 2;
+    this._Y = (n - 1) / 2;
 
     this._imageIdx = imageIdx;
     this._val = val;
   }
 
   update() {
-    return true;
+    let updated = false;
+    if (this._X != this._x) {
+      if (abs(this._x - this._X) < 0.01) {
+        this._X = this._x;
+      } else {
+        this._X = lerp(this._X, this._x, 0.3);
+      }
+      updated = true;
+    }
+    if (this._Y != this._y) {
+      if (abs(this._y - this._Y) < 0.01) {
+        this._Y = this._y;
+      } else {
+        this._Y = lerp(this._Y, this._y, 0.3);
+      }
+      updated = true;
+    }
+    return updated;
   }
 
   draw() {
-    let x = this._x * this._size;
-    let y = this._y * this._size;
+    let imX = (this._val % this._n) * this._size;
+    let imY = floor(this._val / this._n) * this._size;
+    let x = this._X * this._size;
+    let y = this._Y * this._size;
 
     image(
         IMAGES[this._imageIdx],
         x, y, this._size, this._size,
-        x, y, this._size, this._size);
+        imX, imY, this._size, this._size);
     stroke(COLOR.UI_DARK);
     strokeWeight(1);
     noFill();
@@ -30,6 +54,26 @@ class Tile {
     fill(COLOR.UI_DARK);
     textAlign(CENTER, CENTER);
     textSize(30);
-    text(`${this._val}`, x, y, this._size, this._size);
+    text(`${this._val + 1}`, x, y, this._size, this._size);
+  }
+
+  get x() {
+    return this._x;
+  }
+
+  set x(newX) {
+    this._x = newX;
+  }
+
+  get y() {
+    return this._y;
+  }
+
+  set y(newY) {
+    this._y = newY;
+  }
+
+  get value() {
+    return this._val;
   }
 }
--- 15puzzle/scenes.js
+++ 15puzzle/scenes.js
@@ -10,6 +10,8 @@ class GameScene {
 
     this.imageIdx;
     this.tiles;
+    this.emptyX;
+    this.emptyY;
 
     this.resetGame();
   }
@@ -27,6 +29,11 @@ class GameScene {
   draw() {
     background(COLOR.BACKGROUND)
 
+    if (this.gameOver) {
+      image(IMAGES[this.imageIdx], 0, 0, 640, 640);
+      return;
+    }
+
     stroke(COLOR.UI_LIGHT);
     strokeWeight(1);
     fill(COLOR.UI_LIGHT);
@@ -38,19 +45,112 @@ class GameScene {
   }
 
   handleClick(clickX, clickY) {
-    console.log(clickX, clickY);
+    if (this.gameOver) {
+      this.resetGame();
+      return;
+    }
+
+    clickX = floor(clickX / 160);
+    clickY = floor(clickY / 160);
+
+    // Click on the outside
+    if (clickX < 0 || clickX >= 4 || clickY < 0 || clickY >= 4) {
+      return;
+    }
+    // Click on the empty
+    if (clickX == this.emptyX && clickY == this.emptyY) {
+      return;
+    }
+
+    // Do the move
+    for (let tile of this.tiles) {
+      if (tile.x == clickX && tile.y == clickY) {
+        // Move up
+        if (tile.x == this.emptyX && tile.y - 1 == this.emptyY) {
+          this.emptyY = tile.y;
+          tile.y -= 1;
+          break;
+        }
+        // Move right
+        if (tile.x + 1 == this.emptyX && tile.y == this.emptyY) {
+          this.emptyX = tile.x;
+          tile.x += 1;
+          break
+        }
+        // Move down
+        if (tile.x == this.emptyX && tile.y + 1 == this.emptyY) {
+          this.emptyY = tile.y;
+          tile.y += 1;
+          break;
+        }
+        // Move left
+        if (tile.x - 1 == this.emptyX && tile.y == this.emptyY) {
+          this.emptyX = tile.x;
+          tile.x -= 1;
+          break;
+        }
+      }
+    }
+
+    // Check win condition
+    this.gameOver = true;
+    for (let tile of this.tiles) {
+      if (tile.y * 4 + tile.x != tile.value) {
+        this.gameOver = false;
+        break;
+      }
+    }
   }
 
   resetGame() {
     this.gameOver = false;
-
     this.imageIdx = floor(random(IMAGES.length));
 
-    this.tiles = [];
-    for (let i = 0; i < 15; ++i) {
-      this.tiles.push(new Tile(this.imageIdx, i % 4, floor(i / 4), i + 1, 160));
+    // Create puzzle by doing some random moves on tile values
+    let vals = [];
+    let ei = 15;
+    for (let i = 0; i < 16; ++i) {
+      vals.push(i);
+    }
+    for (let i = 0; i < 500; ++i) {
+      const j = random();
+      if (j < 0.25) {
+        // Move up
+        if (ei - 4 >= 0) {
+          vals[ei] = vals[ei - 4];
+          ei -= 4;
+        }
+      } else if (j < 0.5) {
+        // Move right
+        if (floor(ei / 4) == floor((ei + 1) / 4)) {
+          vals[ei] = vals[ei + 1];
+          ei += 1;
+        }
+      } else if (j < 0.75) {
+        // Move down
+        if (ei + 4 < 16) {
+          vals[ei] = vals[ei + 4];
+          ei += 4;
+        }
+      } else {
+        // Move left
+        if (floor(ei / 4) == floor((ei - 1) / 4)) {
+          vals[ei] = vals[ei - 1];
+          ei -= 1;
+        }
+      }
+      vals[ei] = 15;
     }
 
-    // TODO: Mix the tiles
+    // Initialize tiles
+    this.tiles = [];
+    for (let i = 0; i < 16; ++i) {
+      if (vals[i] == 15) {
+        this.emptyX = i % 4;
+        this.emptyY = floor(i / 4);
+      } else {
+        this.tiles.push(new Tile(this.imageIdx, i % 4, floor(i / 4), vals[i], 160, 4));
+      }
+    }
   }
 }
